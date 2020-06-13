cee1ecd0b68a5d81304fda98b6db036efae7cd0a
Lasitha Guruge <lasithaguruge91@gmail.com>
2020-06-13 23:32:24 +0530

    Add item count calculation by UOM functions

--- src/main/java/com/assessment/onlineshop/services/MongoItemService.java
+++ src/main/java/com/assessment/onlineshop/services/MongoItemService.java
@@ -1,14 +1,15 @@
 package com.assessment.onlineshop.services;
 
 import com.assessment.onlineshop.dtos.Item;
+import com.assessment.onlineshop.dtos.Order;
 import com.assessment.onlineshop.repositories.ItemRepository;
+import com.assessment.onlineshop.utill.ItemCountStoreByUOM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class MongoItemService implements ItemService {
@@ -40,6 +41,11 @@ public class MongoItemService implements ItemService {
         return result.orElseThrow(() -> new ItemNotFoundException(id));
     }
 
+    /**
+     * This funcition is calculate the price of a given item
+     * @param item
+     * @return The calculated price
+     */
     public double calculateItemPrice(Item item) {
         if (item == null) throw new NullPointerException();
 
@@ -51,12 +57,55 @@ public class MongoItemService implements ItemService {
         return newCartonPrice / item.getNoOfUnits();
     }
 
-    public double calculateDiscount(Item item, int quantity) {
+    /**
+     * This function calculate the carton discount of a given item
+     * @param item
+     * @return The discount amount
+     */
+    public double getDiscountCartonAmount(Item item) {
         if (item == null) throw new NullPointerException();
 
         double cartonPrice = item.getPrice();
         double discountCartonPrice = cartonPrice - (cartonPrice * 0.1);
 
-        return discountCartonPrice * quantity;
+        return discountCartonPrice;
+    }
+
+    /**
+     * This function is find item count for unit of measure in each item in the order items
+     * @param orderItems
+     * @return The object which contains item id and quantities for each unit of measure
+     */
+    public ArrayList<ItemCountStoreByUOM> getItemCountListByUOM(ArrayList<Order.OrderItem> orderItems) {
+        ArrayList<ItemCountStoreByUOM> countList = new ArrayList<>();
+        Map<String, Integer> cartonItemCountMap = new HashMap<>();
+        Map<String, Integer> singleItemCountMap = new HashMap<>();
+
+        for (int i = 0; i < orderItems.size(); i++) {
+            Order.OrderItem orderItem = orderItems.get(i);
+
+            if (orderItem.getUom().equals("carton")) {
+                updateCountMap(cartonItemCountMap, orderItem);
+            } else {
+                updateCountMap(singleItemCountMap, orderItem);
+            }
+        }
+        return countList;
+    }
+
+    /**
+     * This function is update the given map values by matching the order item id with keys of map
+     * @param map
+     * @param orderItem
+     */
+    public void updateCountMap(Map<String, Integer> map, Order.OrderItem orderItem) {
+        Item item = orderItem.getItem();
+
+        if (map.isEmpty()) {
+            map.put(item.getId(), orderItem.getQuantity());
+        } else {
+            int exisitingCount = map.get(item.getId());
+            map.replace(item.getId(), exisitingCount, exisitingCount + orderItem.getQuantity());
+        }
     }
 }
---
+++ src/main/java/com/assessment/onlineshop/utill/ItemCountStoreByUOM.java
@@ -0,0 +1,25 @@
+package com.assessment.onlineshop.utill;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ItemCountStoreByUOM {
+    private String id;
+    private Map<String, Integer> countByUOM;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, Integer> getCountByUOM() {
+        return countByUOM;
+    }
+
+    public void setCountByUOM(Map<String, Integer> countByUOM) {
+        this.countByUOM = countByUOM;
+    }
+}
